8d4903b02f52ecf568c88b8199b5a58e4cda1aff
Samuel Blebo <samuel.blebo@yahoo.com>
2024-01-07 17:17:10 +0000

    .

--- index.js
+++ index.js
@@ -34,3 +34,9 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+const port = "https://ai-hub-server.vercel.app/";
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
